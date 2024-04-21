50e0188d776e098fbfeaeb8a86adcdbaff339cc6
Khamzeto <147990355+Khamzeto@users.noreply.github.com>
2024-04-21 22:04:32 +0300

    Update email.js

--- smtp/email.js
+++ smtp/email.js
@@ -4,7 +4,7 @@ module.exports = {
   port: 587,
   secure: false, // true for 465, false for other ports
   auth: {
-    user: "iimya266@gmail.com",
-    pass: "ksnz zdqp uuyv dxmw",
+    user: "iimil.com",
+    pass: "ksndxmw",
   },
 };
